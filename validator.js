b8f196eec8b4dcd9746b29f8326e75559e88ab9d
Timo Aho <zeukkari@gmail.com>
2019-01-17 00:21:34 +0200

    remove extra sync call

--- validator.js
+++ validator.js
@@ -1,27 +1,22 @@
-'use strict';
+'use strict'
 
-var ZSchema = require('z-schema');
-var fs = require('fs');
-var path = require('path');
-var schema = require('./schema');
-
-// TODO - Remove this sync call
-var schema = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'schema.json'), 'utf8'));
+var ZSchema = require('z-schema')
+var schema = require('./schema')
 
 function validate(resumeJson, callback) {
   // Callers expect z-schema 2.4.x report object
   var callbackWrapper = function(err, valid) {
-    if(err) {
+    if (err) {
       callback(err)
     } else {
-      callback(null, {valid: valid});
+      callback(null, { valid: valid })
     }
   }
 
-  new ZSchema().validate(resumeJson, schema, callbackWrapper);
+  new ZSchema().validate(resumeJson, schema, callbackWrapper)
 }
 
 module.exports = {
   validate: validate,
-  schema: schema
-};
+  schema: schema,
+}
